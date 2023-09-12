493a17b39a591f3b7edf1f6750564afed26ad21f
bwogit <jeremie.sandot@hotmail.com>
2023-09-12 18:55:42 +0000

    Add products app, add app to settings.py

--- surf_haven/settings.py
+++ surf_haven/settings.py
@@ -21,7 +21,7 @@ if os.path.isfile("env.py"):
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates', 'allauth')
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'home',
+    'products',
 ]
 
 MIDDLEWARE = [
