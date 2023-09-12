c9fee95ac25e02ae2f1208afed27b6bcad9037eb
bwogit <jeremie.sandot@hotmail.com>
2023-09-12 08:52:58 +0000

    add heruku app urls to seetings

--- surf_haven/settings.py
+++ surf_haven/settings.py
@@ -33,7 +33,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-bwogit-cipp5surfhaven-5o9dy0ts1dt.ws-eu104.gitpod.io']
+ALLOWED_HOSTS = ['https://surf-haven-39d68e86c0f1.herokuapp.com/',
+                 '8000-bwogit-cipp5surfhaven-5o9dy0ts1dt.ws-eu104.gitpod.io']
 
 
 # Application definition
@@ -107,12 +108,12 @@ WSGI_APPLICATION = 'surf_haven.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-#DATABASES = {
+# DATABASES = {
 #    'default': {
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': BASE_DIR / 'db.sqlite3',
 #    }
-#}
+# }
 
 DATABASES = {
    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
