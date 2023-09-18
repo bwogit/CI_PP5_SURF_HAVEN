0f478aa49e4d504bac2023d2979d34181193f882
bwogit <jeremie.sandot@hotmail.com>
2023-09-18 10:58:40 +0000

    create views to display all products, add templates and dd entries to urls.py

---
+++ products/urls.py
@@ -0,0 +1,12 @@
+# Imports
+# 3rd party:
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+# Internal:
+from . import views
+
+urlpatterns = [
+    path('', views.all_products, name='products')
+]
