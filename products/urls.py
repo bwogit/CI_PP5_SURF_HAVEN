31bdc3bf68b0420df295f3ffc941703aea08159f
bwogit <jeremie.sandot@hotmail.com>
2023-09-18 17:33:48 +0000

    add urls.py

--- products/urls.py
+++ products/urls.py
@@ -8,5 +8,6 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path('', views.all_products, name='products')
+    path('', views.all_products, name='products'),
+    path('<int:product_id>/', views.product_detail, name='product_detail'),
 ]
