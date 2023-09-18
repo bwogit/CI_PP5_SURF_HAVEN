2fa4d807fc1f53e6e66a9bf7409b7eacaf090c18
bwogit <jeremie.sandot@hotmail.com>
2023-09-18 17:28:01 +0000

    create product detail view

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 # 3rd Party
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.views.generic import ListView
 # Internal
 from .models import Product, Category
@@ -17,3 +17,17 @@ def all_products(request):
     }
 
     return render(request, 'products/product_list.html', context)
+
+
+def product_detail(request, product_id):
+    """ A view to show selected product details """
+
+    product = get_object_or_404(Product, pk=product_id)
+    categories = Category.objects.all()
+
+    context = {
+        'product': product,
+        'categories': categories
+    }
+
+    return render(request, 'products/product_detail.html', context)
