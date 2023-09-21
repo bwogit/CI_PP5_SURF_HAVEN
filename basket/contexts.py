c6b6fdf4aeaa0ad956acd3f73466160c0574c131
bwogit <jeremie.sandot@hotmail.com>
2023-09-21 11:23:23 +0000

    add script to manages the quantity input fields on a webpage for an online store.

--- basket/contexts.py
+++ basket/contexts.py
@@ -13,16 +13,32 @@ def basket_contents(request):
 
     basket = request.session.get('basket', {})
 
-    for item_id, quantity in basket.items():
-        product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
-        product_count += quantity
-        basket_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
-        })
-        
+    for item_id, quantity_data in basket.items():
+        # Check if 'quantity' is a dictionary
+        if isinstance(quantity_data, dict):
+            # Extract the numeric value from the 'quantity' dictionary
+            quantity = quantity_data.get('value', 0)  # Replace 'value' with the correct key
+        else:
+            # If 'quantity' is not a dictionary, assume it's a numeric value
+            quantity = quantity_data
+
+    try:
+        product = Product.objects.get(pk=item_id)
+    except Product.DoesNotExist:
+        raise Http404("Product not found")
+
+    item_total = quantity * product.price
+    total += item_total
+    product_count += quantity
+
+    basket_item = {
+        'item_id': item_id,
+        'quantity': quantity,
+        'product': product,
+        'item_total': item_total,
+    }
+    basket_items.append(basket_item)
+
     for item_id, item_data in basket.items():
         if isinstance(item_data, int):
             product = get_object_or_404(Product, pk=item_id)
@@ -46,7 +62,7 @@ def basket_contents(request):
                 })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE/100)
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
