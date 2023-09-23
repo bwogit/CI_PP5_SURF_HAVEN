1a2a3072f5deea5f20247852865e02a81d77da1b
bwogit <jeremie.sandot@hotmail.com>
2023-09-23 11:17:44 +0000

    Regirter models in admins.py

--- checkout/models.py
+++ checkout/models.py
@@ -8,21 +8,21 @@ from products.models import Product
 
 
 class Order(models.Model):
-    order_number = models.CharField(max_length=32, null=False, editable=False)
-    full_name = models.CharField(max_length=50, null=False, blank=False)
-    email = models.EmailField(max_length=254, null=False, blank=False)
-    phone_number = models.CharField(max_length=20, null=False, blank=False)
-    town_or_city = models.CharField(max_length=40, null=False, blank=False)
-    address1 = models.CharField(max_length=80, null=False, blank=False)
-    address2 = models.CharField(max_length=80, null=True, blank=True)
-    county = models.CharField(max_length=80, null=True, blank=True)
-    postcode = models.CharField(max_length=20, null=True, blank=True)
-    country = models.CharField(max_length=40, null=False, blank=False)
+    order_number = models.CharField(max_length=32, null=False, editable=False, default='')
+    full_name = models.CharField(max_length=50, null=False, blank=False, default='')
+    email = models.EmailField(max_length=254, null=False, blank=False, default='' )
+    phone_number = models.CharField(max_length=20, null=False, blank=False, default='')
+    town_or_city = models.CharField(max_length=40, null=False, blank=False, default='')
+    address1 = models.CharField(max_length=80, null=False, blank=False, default='')
+    address2 = models.CharField(max_length=80, null=True, blank=True, default='')
+    county = models.CharField(max_length=80, null=True, blank=True, default='')
+    postcode = models.CharField(max_length=20, null=True, blank=True, default='')
+    country = models.CharField(max_length=40, null=False, blank=False, default='')
     date = models.DateTimeField(auto_now_add=True)
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
-    original_bag = models.TextField(null=False, blank=False, default='')
+    original_basket = models.TextField(null=False, blank=False, default='')
     stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
 
     def _generate_order_number(self):
