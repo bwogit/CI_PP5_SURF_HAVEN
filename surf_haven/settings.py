109eb1a06cc55f1fbc6e9b224ebf3afd01eee966
bwogit <jeremie.sandot@hotmail.com>
2023-09-27 15:09:58 +0000

    Remove cloudinary from installed apps

--- surf_haven/settings.py
+++ surf_haven/settings.py
@@ -62,7 +62,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'cloudinary_storage',
     'django.contrib.staticfiles',
-    'cloudinary',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
