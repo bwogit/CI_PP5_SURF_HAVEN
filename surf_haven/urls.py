e2b7e337ced2a81ee9251ffa298e8e304b7dc185
bwogit <jeremie.sandot@hotmail.com>
2023-09-28 15:04:05 +0000

    move contact urls above blog

--- surf_haven/urls.py
+++ surf_haven/urls.py
@@ -8,12 +8,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
+    path('', include('contact.urls')),
     path('products/', include('products.urls')),
     path('basket/', include('basket.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
     path('summernote/', include('django_summernote.urls')),
     path('', include('blog.urls')),
-    path('', include('contact.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 handler404 = 'surf_haven.views.handler404'
