d3de2aea733a331ed982df572681ff25cdb33f91
bwogit <jeremie.sandot@hotmail.com>
2023-10-02 11:24:19 +0000

    change inmport datetime and add correct URL in school_details

--- bookings/views.py
+++ bookings/views.py
@@ -7,7 +7,7 @@ from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import UpdateView
 from django.core.paginator import Paginator
-import datetime
+from datetime import datetime
 # Internal:
 from .models import School, Booking
 from .forms import BookingForm
