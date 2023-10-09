0ffade72ca4268ee179a93edb5d5580e362ad73b
bwogit <jeremie.sandot@hotmail.com>
2023-10-09 13:56:13 +0000

    Add text sucess to checjkout, adjust basket title

--- blog/forms.py
+++ blog/forms.py
@@ -1,12 +1,16 @@
-# Imports
+# 3rd part Imports
 from django import forms
-# Internal:
+# Internal imports
 from . models import Comment
 
-# Registered user Comment form 
+# Registered user Comment form
 
 
 class CommentForm(forms.ModelForm):
+    """
+    A form for registered users to leave comments on blog posts.
+    """
+
     class Meta:
         model = Comment
         fields = ('body',)
