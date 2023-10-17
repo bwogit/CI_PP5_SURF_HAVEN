8af66ed99b8cd7605ebc3da7ad53a4fa5329054d
bwogit <jeremie.sandot@hotmail.com>
2023-10-17 14:50:02 +0000

    troubleshooting taost scroll issue - unresolved

--- profiles/models.py
+++ profiles/models.py
@@ -1,11 +1,10 @@
-# Imports
-# 3rd Party
+# 3rd Party Imports
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django_countries.fields import CountryField
-# Internal
+# Internal imports
 
 
 class UserProfile(models.Model):
