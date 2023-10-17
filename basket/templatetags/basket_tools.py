e6050c7ec4163a5b38ef1047f0c854721637f636
bwogit <jeremie.sandot@hotmail.com>
2023-10-17 10:57:49 +0000

    use same quantity logic and buttons from product_details to quantity_form.html

--- basket/templatetags/basket_tools.py
+++ basket/templatetags/basket_tools.py
@@ -1,8 +1,8 @@
-# Imports
-# 3rd party:
+# 3rd Party Imports
 from django import template
 # Internals
 register = template.Library()
+
 
 @register.filter(name='calc_subtotal')
 def calc_subtotal(price, quantity):
