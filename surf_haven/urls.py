efaef557a9511528431dcec4cc981d72af6258be
bwogit <jeremie.sandot@hotmail.com>
2023-10-24 14:31:22 +0000

    add a 500 handler

--- surf_haven/urls.py
+++ surf_haven/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 # Internal Imports
-from .views import handler404
+from .views import handler404, handler500
 
 urlpatterns = [
     path('admin/', admin.site.urls),
